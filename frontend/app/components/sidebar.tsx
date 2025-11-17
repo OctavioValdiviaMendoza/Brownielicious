c728944dd39ab7314715d00a3bf017a5cedf6ab4
OctavioValdiviaMendoza <octavio.valdiviamendoza@sjsu.edu>
2025-11-17 12:47:36 -0800

    added a sidebar that will be the main  directory also uploaded image of logo

--- frontend/app/components/sidebar.tsx
+++ frontend/app/components/sidebar.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
+import Image from 'next/image'
+import {Lily_Script_One} from 'next/font/google'
+
+const lily = Lily_Script_One({subsets: ['latin'], weight: '400'})
 export default function Sidebar() {
     return (
-        <div className="bg-pink-400 md:h-full">
-            <div>Brownielicious</div>
-            <div>Sidebar</div>
+        <div className="bg-[#FC51AF] w-[300px] min-h-screen p-4  place-content-start flex flex-col 
+        flex-start gap-4 border-black border-2 rounded" >
+            <div className="flex flex-row gap-4">
+                <Image className= "mb-4 border-black border-1 rounded" 
+                src="/brownieliciousLogo.jpg" alt="Brownielicious Logo" width={40} height={40} />
+                <Link className ={`${lily.className} text-white text-[30px]`} href="/Home">Brownielicious</Link>
+            </div>
+            <Link href="" className ={`${lily.className} text-white text-[30px] `}>Brownie Boxes</Link>
+            <Link href="" className ={`${lily.className} text-white text-[30px]`}>Catering</Link>
+            <Link href="" className ={`${lily.className} text-white text-[30px]`}>Order History</Link>
         </div>
     );
 }
