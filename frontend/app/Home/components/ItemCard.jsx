aac49f347b09d0e3166ecc3a7e8fa60230d3eccd
Leonardo Garcia <garciaherreraleonardo29@gmail.com>
2025-11-26 15:04:16 -0800

    Added background image and styling to item card

--- frontend/app/Home/components/ItemCard.jsx
+++ frontend/app/Home/components/ItemCard.jsx
@@ -1,17 +1,27 @@
-export default function ItemCard() {
+import { Lily_Script_One } from "next/font/google";
+
+const Lily_Script_One_font = Lily_Script_One({
+    weight: '400',
+    subsets: ['latin'],
+});
+
+export default function ItemCard({ bgImage, itemImage, itemName, itemDescription }) {
     return (
-        <div className="bg-red-800 w-9/12 flex flex-row justify-center p-4 gap-18">
-            <div className="bg-white flex justify-center">
-                <img src="/bronie.png" alt="Oreo Brownie" className="w-10/12"/>
-            
-                
+
+        <div className={`w-9/12 flex flex-row justify-center 
+        gap-10 rounded-xl transition-shadow hover:shadow-2xl hover:shadow-black/60 m-10`}
+            style={{ backgroundImage: `url('${bgImage}')` }}>
+            <div className=" flex justify-center m-3">
+                <img src={itemImage} alt="Oreo Brownie" className="w-7/12 shadow-xl shadow-black/100" />
+
+
             </div>
-            <div className="bg-blue-500 w-4/12">
-                <div>
-                    Oreo Brownie
+            <div className="w-4/12 bg-black/25 rounded-r-xl hover:bg-black/40 transition-colors duration-300">
+                <div className="text-center text-2xl text-black mt-17">
+                    <p className={`${Lily_Script_One_font.className}`}>{itemName}</p>
                 </div>
-                <div>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus in ornare quam viverra orci sagittis eu volutpat. Pharetra sit amet aliquam id diam maecenas ultricies mi. Eget arcu dictum varius duis at consectetur lorem donec massa. Morbi tristique senectus et netus et malesuada fames ac turpis.
+                <div className="text-white text-center p-4">
+                    {itemDescription}
                 </div>
             </div>
         </div>
--- frontend/app/Home/page.tsx
+++ frontend/app/Home/page.tsx
@@ -4,7 +4,16 @@ import "../globals.css";
 
 
 export default function Home() {
+
+    const name: string = "Chocolate Brownie"
+    const description: string = "Delicious chocolate brownie with a rich and fudgy texture, topped with a drizzle of chocolate ganache.ocolate brownie with a rich and fudgy texture, topped with a drudgy texture, topped with a drizzle of chocolate"
+    const bgImage: string = "/Chocolate_Bg_ItemCard.jpeg"
+    const itemImage: string = "/bronie.png"
+
     return (<>
-        <ItemCard />
+        <ItemCard bgImage={itemImage} itemImage={itemImage} itemName={name} itemDescription={description} />
+        <ItemCard bgImage={bgImage} itemImage={itemImage} itemName={name} itemDescription={description} />
+
+
     </>)
 }
